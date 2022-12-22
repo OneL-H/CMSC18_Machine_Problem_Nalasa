ea4a964aa5becc7aa4f1e09cdbcce11a386ccf6a
OneL-H <117259320+OneL-H@users.noreply.github.com>
2022-12-22 19:57:57 +0800

    okay we're at the lines part
    so here we go

--- CMSC18_Machine_Problem_Nalasa.c
+++ CMSC18_Machine_Problem_Nalasa.c
@@ -25,28 +25,52 @@ int square(int i){
     return i*i;
 }
 
-int radius;
+int radius; // global radius variable so i dont need to keep passing it
 
-void draw_Graph(int radius, char graph[2*radius+1][2*radius+1]){
-    int y, x;
-    for(y = 0; y<(2*radius+1); ++y){
-        printf("\n");
-        for(x = 0; x < radius*2+1; x++){
-            printf("%c", graph[x][y]);
-        }
-    }
-}
+void draw_circle(char (*graph)[2*radius+1]);
+void draw_Graph(int radius, char graph[2*radius+1][2*radius+1]);
 
 void set_slope(line *segment){
-    segment->slope = (float)((segment->endpoint_1.y - segment->endpoint_2.y) / (segment->endpoint_1.x - segment->endpoint_2.x));
+    printf("\nm = (%d - %d) / (%d - %d)", segment->endpoint_1.y, segment->endpoint_2.y, segment->endpoint_1.x, segment->endpoint_2.x);
+    segment->slope = (((float)segment->endpoint_1.y - (float)segment->endpoint_2.y) / ((float)segment->endpoint_1.x - (float)segment->endpoint_2.x));
+    printf(" = %f", segment->slope);
 }
 
-void draw_point(point endpoint, char ch, char graph [2*radius+1][2*radius+1]){
-    // printf("\nchanging %c at (%d, %d) to %c", graph[endpoint.y][endpoint.x], endpoint.x, endpoint.y, ch);
-    printf("\n(%d, %d)", endpoint.x, endpoint.y);
-    // graph[endpoint.y][endpoint.x] = ch;
+void draw_point(point *endpoint, char ch, char (*graph)[2*radius+1]){
+    printf("\nchanging %c at (%d, %d) to %c", *(*(graph + endpoint->x) + endpoint->y), endpoint->x, endpoint->y, ch);
+    // printf("\n(%d, %d)", endpoint.x, endpoint.y);
+    *(*(graph + endpoint->x) + endpoint->y) = ch;
 }
 
+    // since the slope has been predefined in the one-dimensional struct
+    // we can use the "point-slope form" of a line: (y - y1) = m(x - x1)
+    // rearranging to find y, we get: y = m(x - x1) + y1
+    // for x, its a bit more complicated than the circle y - y1 = mx - mx1
+    // mx = y - y1 + mx1 => x = (y - y1 + mx1) / m
+    // idk maybe we'll use differenct endpoints for x1 and y1 maybe that will improve 
+    // resolution
+
+void draw_line(line segment, char (*graph)[2*radius+1], char ch){
+    int start_x = segment.endpoint_1.x > segment.endpoint_2.x ? segment.endpoint_2.x : segment.endpoint_1.x;
+    int end_x = segment.endpoint_1.x < segment.endpoint_2.x ? segment.endpoint_2.x : segment.endpoint_1.x;
+
+    int start_y = segment.endpoint_1.y > segment.endpoint_2.y ? segment.endpoint_2.y : segment.endpoint_1.y;
+    int end_y = segment.endpoint_1.y < segment.endpoint_2.y ? segment.endpoint_2.y : segment.endpoint_1.y;
+
+    // pass 1: pass through the x values
+    for(int x = start_x; x < end_x+1; x++){
+        printf("\n(%d, %d)", x, (int)round(segment.slope*(start_x - x) + start_y));
+        *(*(graph + x) + (int)round(segment.slope*(start_x - x) + start_y)) = ch;
+    }
+
+    // pass 2: pass through the y values
+    if(segment.slope != 0){
+        for(int y = start_y; y < end_y+1; y++){
+            printf("\n(%d, %d)", (int)round((y - start_y + segment.slope*start_x) / segment.slope), y);
+            *(*(graph + (int)round((y - start_y + segment.slope*start_x) / segment.slope)) + y) = ch;
+        }
+    } // one line is straight so we dont touch that.
+}
 
 int main(){
     
@@ -65,49 +89,38 @@ int main(){
         }
     }
     
-    // okay the formula for a circle is (x-h)^2 + (y-i)^2 = R^2
-    // rearranging the terms, we get (y-i)^2 = R^2 - (x-h)^2
-    // then, y-i = +/- sqrt(R^2 - (x-h)^2)
-    // where y = i +/- sqrt(R^2 - (x-h)^2)
-    // therefore, we can cycle through all x values from 0 to radius*2 + 1
-    // and find all coordinates to fill with...something
+    draw_circle(graph);
 
-    for(int x = 0; x < radius*2+1; x++){
-        // printf("\n(%d, %d)", x, (int)round(radius + sqrt(square(radius) - square(x-radius))));
-        // printf("\n(%d, %d)", x, (int)round(radius - sqrt(square(radius) - square(x-radius))));
-        graph[(int)round(radius + sqrt(square(radius) - square(x-radius)))][x] = '#';
-        graph[(int)round(radius - sqrt(square(radius) - square(x-radius)))][x] = '#';
-    }
-
-    // swap the values for a second pass - literally the same thing lmao cause they're interchangable
-    // makes the circle more "continuous" i guess?
-    for(int y = 0; y < radius*2+1; y++){
-        // printf("\n(%d, %d)", (int)round(radius + sqrt(square(radius) - square(y-radius))), y);
-        // printf("\n(%d, %d)", (int)round(radius - sqrt(square(radius) - square(y-radius))), y);
-        graph[y][(int)round(radius + sqrt(square(radius) - square(y-radius)))] = '#';
-        graph[y][(int)round(radius - sqrt(square(radius) - square(y-radius)))]= '#';
-    } // p.s. oh wow yeah that worked really well
-    
     draw_Graph(radius, graph);
-    
-    // uhh maam the reason these functions are all here is because i dont know how to pass
-    // by reference with a two-dimensional array
 
     point point_A, point_B, point_C, point_D, point_E;
     
     const int base_angle = 18;
     
-    point points[5] = {point_A, point_B, point_C, point_D, point_E};
-    char letters[5] = {'A', 'B', 'C', 'D', 'E'};
-    for(int i = 0; i < 5; i++){
-        points[i].x = (int)round(radius * cos(((i*72 + base_angle) * pi) / 180) + radius);
-        points[i].y = (int)round(radius * sin(((i*72 + base_angle) * pi) / 180) + radius);
-        printf("\n%c (%d, %d)", letters[i], points[i].x, points[i].y);
-    }
-    /*for(int i = 0; i < 5; i++){
-        draw_point(points[i], letters[i], graph);
-    }*/
+    // maam. i dont know why THIS method works okay but if i put it in a for loop,
+    // accessing it with points[i].x, the numbers all turn big for some reason
+    // so when i try to feed it into other functions, i end up trying to access a cell in graph
+    // that is out of bounds. weird C magic i dont know yet, i guess.
     
+    point_A.x = (int)round(radius * cos(((base_angle) * pi) / 180) + radius);
+    point_A.y = abs((int)round(radius * sin(((base_angle) * pi) / 180) - radius));
+    draw_point(&point_A, 'A', graph);
+    
+    point_B.x = (int)round(radius * cos(((1*72 + base_angle) * pi) / 180) + radius);
+    point_B.y = abs((int)round(radius * sin(((1*72 + base_angle) * pi) / 180) - radius));
+    draw_point(&point_B, 'B', graph);
+
+    point_C.x = (int)round(radius * cos(((2*72 + base_angle) * pi) / 180) + radius);
+    point_C.y = abs((int)round(radius * sin(((2*72 + base_angle) * pi) / 180) - radius));
+    draw_point(&point_C, 'C', graph);
+
+    point_D.x = (int)round(radius * cos(((3*72 + base_angle) * pi) / 180) + radius);
+    point_D.y = abs((int)round(radius * sin(((3*72 + base_angle) * pi) / 180) - radius));
+    draw_point(&point_D, 'D', graph);
+
+    point_E.x = (int)round(radius * cos(((4*72 + base_angle) * pi) / 180) + radius);
+    point_E.y = abs((int)round(radius * sin(((4*72 + base_angle) * pi) / 180) - radius));
+    draw_point(&point_E, 'E', graph);
     
     // draw_Graph(radius, graph);
 
@@ -136,23 +149,47 @@ int main(){
     segment_CE.endpoint_2 = point_E;
     set_slope(&segment_CE);
     
-    line segments[5] = {segment_AC, segment_AD, segment_BE, segment_CE, segment_BD};
-    /*
-    for(int i = 0; i < 5; i++){
-        draw_line(segments[i], );
-    }
-    */
     // okay, so these are the endpoints of the star. we will basically do the same thing
     // with the circle but the formula is different this time.
     
-    // since the slope has been predefined in the one-dimensional struct
-    // we can use the "point-slope form" of a line: (y - y1) = m(x - x1)
-    // rearranging to find y, we get: y = m(x - x1) + y1
-    // for x, its a bit more complicated than the circle y - y1 = mx - mx1
-    // mx = y - y1 + mx1 => x = (y - y1 + mx1) / m
-    // idk maybe we'll use differenct endpoints for x1 and y1 maybe that will improve 
-    // resolution
+    draw_line(segment_AC, graph, 'A');
+    draw_line(segment_AD, graph, 'D');
+    draw_line(segment_BD, graph, 'B');
+    draw_line(segment_BE, graph, 'E');
+    draw_line(segment_CE, graph, 'C');
 
+    draw_Graph(radius, graph);
 
     return 0;
+}
+
+// okay the formula for a circle is (x-h)^2 + (y-i)^2 = R^2
+// rearranging the terms, we get (y-i)^2 = R^2 - (x-h)^2
+// then, y-i = +/- sqrt(R^2 - (x-h)^2)
+// where y = i +/- sqrt(R^2 - (x-h)^2)
+// therefore, we can cycle through all x values from 0 to radius*2 + 1
+// and find all coordinates to fill with...something
+
+void draw_circle(char (*graph)[2*radius+1]){
+    for(int x = 0; x < radius*2+1; x++){
+        *(*(graph + x) + (int)round(radius + sqrt(square(radius) - square(x-radius)))) = '#';
+        *(*(graph + x) + (int)round(radius - sqrt(square(radius) - square(x-radius)))) = '#';
+    }
+
+    // swap the values for a second pass - literally the same thing lmao cause they're interchangable
+    // makes the circle more "continuous" i guess?
+    for(int y = 0; y < radius*2+1; y++){
+        *(*(graph + (int)round(radius + sqrt(square(radius) - square(y-radius)))) + y) = '#';
+        *(*(graph + (int)round(radius - sqrt(square(radius) - square(y-radius)))) + y) = '#';
+    } // p.s. oh wow yeah that worked really well
+}
+
+void draw_Graph(int radius, char graph[2*radius+1][2*radius+1]){
+    int y, x;
+    for(y = 0; y<(2*radius+1); ++y){
+        printf("\n");
+        for(x = 0; x < radius*2+1; x++){
+            printf("%c", graph[x][y]);
+        }
+    }
 }
